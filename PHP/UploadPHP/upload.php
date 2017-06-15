d801dc7ed9883c9ecb9974ceae6d5cbb21c48c42
Hugo-PC\Hugo <dancinoman@gmail.com>
2017-06-14 20:01:00 -0400

    PHP upload
    PHP upload file was added

---
+++ PHP/UploadPHP/upload.php
@@ -0,0 +1,51 @@
+
+<?php
+if(isset($_FILES["fileToUpload"])){
+    $target_dir = "uploads/";
+    $target_file = $target_dir. basename($_FILES["fileToUpload"]["name"]);
+    $uploadOK = 1;
+    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+}
+   //check if image file is actual image or fake image
+
+if(isset($_POST["submit"])) {
+     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+      if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOK = 1;
+      } else {
+           echo "File is not an image";
+           $uploadOK = 0;
+      }
+
+      if (file_exists($target_file)) {
+        echo "Sorry, file already exists.";
+        $uploadOK = 0;
+      }
+
+      if($_FILES["fileToUpload"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOK = 0;
+      }
+
+      if($imageFileType != "jpg" && $imageFileType !="png" && $imageFileType !="jpeg"
+      && $imageFileType != "gif") {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOK = 0;
+      }
+
+      //Check if $uploadOK is set to 0 by an error
+      if ($uploadOK == 0) {
+        echo "Sorry, your file was not uploaded.";
+      } else {
+        if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+          echo "The file" . basename($_FILES["fileToUpload"]["name"]) . "has been dowloaded";
+      }
+      else {
+        echo "Sorry, there was an error uploading file.";
+      }
+    }
+}
+
+
+ ?>
